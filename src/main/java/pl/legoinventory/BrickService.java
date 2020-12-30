dc00e7946192708f4b430a66414180979373eb51
Dominik Knap <dominik.knap.93@gmail.com>
2020-12-30 23:12:39 +0100

    adding delete by number and update by number methods

--- src/main/java/pl/legoinventory/BrickService.java
+++ src/main/java/pl/legoinventory/BrickService.java
@@ -5,10 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,5 +32,27 @@ public class BrickService {
         }
         Brick savedBrick = brickRepository.save(brick);
         return ResponseEntity.ok(savedBrick);
+    }
+
+    @PutMapping("/bricks/{number}")
+    public ResponseEntity updateBrickNumber(@RequestBody Brick brick, @PathVariable("number")String number){
+        List<Brick> brickFromDb = brickRepository.findAllByNumber(number);
+        if (brickFromDb.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        Brick brickToUpdate = brickFromDb.get(0);
+        brickToUpdate.setNumber(brick.getNumber());
+        brickRepository.save(brickToUpdate);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @DeleteMapping("/bricks/{number}")
+    public ResponseEntity removeBrick(@PathVariable("number")String number) {
+        List<Brick> brickFromDb = brickRepository.findAllByNumber(number);
+        if (brickFromDb.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
+        brickRepository.delete(brickFromDb.get(0));
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 }
